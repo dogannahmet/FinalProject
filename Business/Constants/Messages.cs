711e924a1d72c4c3f32d2de375cee9adbc2e7a97
Ahmet D <ahmet.msn@live.com>
2021-04-23 23:35:26 +0300

    Update

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Constants
+{
+    // static olmasının nedeni new()'lenmesine ihtiyaç duyulmaması için. Çünkü sabit bölüm.
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün eklendi.";
+        public static string ProductNameInvalid = "Ürün ismi geçersiz.";
+        public static string MaintenanceTime = "Sistem bakımda.";
+        public static string ProductsListed = "Ürünler listelendi.";
+    }
+}
---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        // this => class'ın kendisi demek(result)
+        // :this(success) => result'un tek parametreli constructor'ına success gönder.(yani iki constructor'da çalışır.)
+        public Result(bool success, string message) : this(success)
+        {
+            //get => readonly..
+            //readonly yapılar, constructor'da set edilebilir...
+            
+          //Success = success;    //Don't repeat yourself..
+            Message = message;
+
+        }
+
+        public Result(bool success)
+        {
+            Success = success;
+        }
+
+        public bool Success { get; }
+
+        public string Message { get; }
+    }
+}
